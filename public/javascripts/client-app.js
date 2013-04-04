97ea21f8fd53b96cbb03101e18146c339911682d
Arwen Hawes <ahawes@unca.edu>
2013-04-04 12:51:15 -0400

    add messy but workable loop. clean it up later

--- public/javascripts/client-app.js
+++ public/javascripts/client-app.js
@@ -1,8 +1,17 @@
 var main = function () {
-   console.log("hello world!"); 
-   $.getJSON("/counts.json", function (response) {
-        $("body").append("<p>awesome: "+response.awesome+"</p>");
+  var happyWords = ["happy", "cheerful","chipper","content","ecstatic","elated","glad","joyful","merry","pleased"],
+      sadWords = ["sad","dismal","distressed","heartbroken","morbid","morose","somber","sorrowful","glum","dejected"];
+  function consoleResults(response){
+    console.log(response);
+  }
+  function wordCounts(response){
+    $("body").append("<p>"+response.key+" : "+response.value+"</p>");
+  }
+  console.log("hello world! Yay!"); 
+   $.getJSON("/counts.json", function (response) {    
+    response.forEach(wordCounts);      
     });
 };
 
+//original was: $("body").append("<p>happy: "+response.happy+"</p>");
 $(document).ready(main);
